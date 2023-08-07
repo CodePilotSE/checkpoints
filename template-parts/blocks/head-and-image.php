b4718b4de69a96387a62d3545fd4deba6e6004af
FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>
2023-08-07 08:53:18 +0200

    new title block
    Signed-off-by: FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>

--- template-parts/blocks/head-and-image.php
+++ template-parts/blocks/head-and-image.php
@@ -10,6 +10,6 @@ $title = get_field('title-block_title');
 $image = get_field('title-block_bg-img');
 
 ?>
-<section class="block-wrapper image-block"  style=" <?= $image ? ' background-image: url('. $image .');' : '' ?> " >
+<section class="block-wrapper image-title-block"  style=" <?= $image ? ' background-image: url('. $image .');' : '' ?> " >
     <?= $title  ? '<h2>'. $title  . '</h2>'   : '' ?>
 </section>
