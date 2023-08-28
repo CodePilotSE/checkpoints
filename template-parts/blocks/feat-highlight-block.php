858777424a802d2edffadd4602b9b6547659eeb4
FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>
2023-08-28 09:06:59 +0200

    Styling feature highlights
    Signed-off-by: FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>

--- template-parts/blocks/feat-highlight-block.php
+++ template-parts/blocks/feat-highlight-block.php
@@ -2,17 +2,17 @@
 $classes = [];
 $classes[] = 'feature-highlight-block';
 $classes[] = $block['className'];
-if ( ! empty( $block['backgroundColor'] ) ) {
+if (!empty($block['backgroundColor'])) {
   $classes[] = 'has-background';
   $classes[] = 'has-' . $block['backgroundColor'] . '-background-color';
 }
-if ( ! empty( $block['gradient'] ) ) {
+if (!empty($block['gradient'])) {
   $classes[] = 'has-gradient';
-  $classes[] = 'has-'.$block['gradient'].'-background';
+  $classes[] = 'has-' . $block['gradient'] . '-background';
 }
 
-$block['style']['color']['gradient'] ? $background = $block['style']['color']['gradient']: '';
-$block['style']['color']['background'] ? $background = $block['style']['color']['background']: '';
+$block['style']['color']['gradient'] ? $background = $block['style']['color']['gradient'] : '';
+$block['style']['color']['background'] ? $background = $block['style']['color']['background'] : '';
 ?>
 <?php
 $title = get_field('feat-highlight-text-group')['feat-highlight-title'];
@@ -21,12 +21,15 @@ $image = get_field('feat-highlight-image');
 $size = 'full'; // (thumbnail, medium, large, full or custom size)
 
 ?>
-<section class="<?= esc_attr( join( ' ', $classes ) ) ?>" <?= $background ? 'style="background:'. $background .';"':'' ?> >
-  <div class="feature-highlight-block--inner">
-    <div class="feature-highlight-block--text-side">
-      <h3 class="feature-highlight-block--title">    <?= $title ? $title : '' ?>     </h3>
-      <p class="feature-highlight-block--content">  <?= $content ? $content : '' ?> </p>
+<section class="<?= esc_attr(join(' ', $classes)) ?>" <?= $background ? 'style="background:' . $background . ';"' : '' ?>>
+
+  <div class="feature-highlight-block__inner">
+    <div class="feature-highlight-block__text-side">
+      <h3 class="feature-highlight-block__title"> <?= $title ? $title : '' ?> </h3>
+      <p class="feature-highlight-block__content"> <?= $content ? $content : '' ?> </p>
     </div>
-    <?= $image ? wp_get_attachment_image( $image, $size ) : '' ?>
+    <div class="feature-highlight-block__image-side">
+      <?= $image ? wp_get_attachment_image($image, $size, "", array("class" => "feature-highlight-block__image")) : '' ?>
+    </div>
   </div>
 </section>
