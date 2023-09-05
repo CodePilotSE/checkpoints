31f45a4b2228cc6a7bea73fc63ab7ad835b9cf01
FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>
2023-09-05 09:33:38 +0200

    rebuilt to alternative design
    Signed-off-by: FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>

--- template-parts/blocks/feat-highlight-block.php
+++ template-parts/blocks/feat-highlight-block.php
@@ -18,18 +18,19 @@ $block['style']['color']['background'] ? $background = $block['style']['color'][
 $title = get_field('feat-highlight-text-group')['feat-highlight-title'];
 $content = get_field('feat-highlight-text-group')['feat-highlight-content'];
 $image = get_field('feat-highlight-image');
-$size = 'full'; // (thumbnail, medium, large, full or custom size)
+$size = 'feature-heighlight'; // (thumbnail, medium, large, full or custom size)
 
 ?>
 <section class="<?= esc_attr(join(' ', $classes)) ?>" <?= $background ? 'style="background:' . $background . ';"' : '' ?>>
 
   <div class="feature-highlight-block__inner">
+    
+    <div class="feature-highlight-block__image-side">
+      <?= $image ? wp_get_attachment_image($image, $size, "", array("class" => "feature-highlight-block__image")) : '' ?>
+    </div>
     <div class="feature-highlight-block__text-side">
       <h3 class="feature-highlight-block__title"> <?= $title ? $title : '' ?> </h3>
       <p class="feature-highlight-block__content"> <?= $content ? $content : '' ?> </p>
-    </div>
-    <div class="feature-highlight-block__image-side">
-      <?= $image ? wp_get_attachment_image($image, $size, "", array("class" => "feature-highlight-block__image")) : '' ?>
     </div>
   </div>
 </section>
