63176592122386ff9fc5a8c510c26783089ebac2
FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>
2024-01-16 08:17:01 +0100

    Styling testimonials block
    Signed-off-by: FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>

--- template-parts/blocks/testimonials-block.php
+++ template-parts/blocks/testimonials-block.php
@@ -2,7 +2,7 @@
 $size = 'thumbnail';
 
 $classes = [];
-$classes[] = 'testimonials-block';
+$classes[] = 'testimonials-wrapper';
 if ( ! empty( $block['backgroundColor'] ) ) {
   $classes[] = 'has-background';
   $classes[] = 'has-' . $block['backgroundColor'] . '-background-color';
@@ -16,7 +16,7 @@ $block['style']['color']['gradient'] ? $background = $block['style']['color']['g
 $block['style']['color']['background'] ? $background = $block['style']['color']['background']: '';
 ?>
 <section class="<?= esc_attr( join( ' ', $classes ) ) ?>" <?= $background ? 'style="background:'. $background .';"':'' ?> >
-  <div class="testamonials">
+  <div class="testimonials-block">
     <?php 
     // build post type and query it
     $args = array(
@@ -26,9 +26,6 @@ $block['style']['color']['background'] ? $background = $block['style']['color'][
       );
     $the_query = new WP_Query( $args );
     
-    echo '<pre>';
-    // print_r($the_query);
-    echo '</pre>';
     if ( $the_query->have_posts() ) : 
       while ( $the_query->have_posts() ) :
         $the_query->the_post();
@@ -37,12 +34,16 @@ $block['style']['color']['background'] ? $background = $block['style']['color'][
         $name    =  get_field('testimonial-name', $post->ID);
         $company =  get_field('testimonial-company', $post->ID);
         ?>
-        <div class="testamonials">
-          <h6><?= get_the_title() ?></h6>  
+        <div class="testimonials-single">
+          <h6 class="testimonials-single__quote"><?= get_the_title() ?></h6>  
 
-          <?= get_the_post_thumbnail('','thumbnail') ?>
-          <?= !empty($name)    ? '<span>'. $name    . '</span>' : '' ?>
-          <?= !empty($company) ? '<span>'. $company . '</span>' : '' ?>
+          <div class="testimonials-single__citee">
+            <?= get_the_post_thumbnail('','thumbnail', array( 'class' => 'testimonials-single__image')) ?>
+            <div class="testimonials-single__name-and-company">
+              <?= !empty($name)    ? '<span class="testimonials-single__name">'. $name    . '</span>' : '' ?>
+              <?= !empty($company) ? '<span class="testimonials-single__company">'. $company . '</span>' : '' ?>
+            </div>
+          </div>
         </div>
       <?php endwhile?>
     <?php endif?>
