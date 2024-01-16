d3f45531135a8b1c4bcfe8da9ab5c167b4716d1a
FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>
2024-01-16 14:34:59 +0100

    added clients block
    Signed-off-by: FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>

--- template-parts/blocks/clients-block.php
+++ template-parts/blocks/clients-block.php
@@ -16,7 +16,21 @@ $block['style']['color']['gradient'] ? $background = $block['style']['color']['g
 $block['style']['color']['background'] ? $background = $block['style']['color']['background']: '';
 ?>
 <section class="<?= esc_attr( join( ' ', $classes ) ) ?>" <?= $background ? 'style="background:'. $background .';"':'' ?> >
-  <div class="clients-block__inner">
-    
-  </div>
+  <?php if( have_rows('clients-repeater') ): ?>
+    <div class="clients-block__inner">
+      <?php
+      while( have_rows('clients-repeater') ) : the_row();
+        $link = get_sub_field('client-link');
+        $logo = get_sub_field('client-logo');
+        if(!empty($link) && !empty($logo)):
+          ?>
+          <a class="clients-block__link" target="_blank" href="<?= $link ?>">
+            <?= wp_get_attachment_image($logo, 'medium') ?>
+          </a>
+          <?php
+        endif;
+      endwhile;
+      ?>
+    </div>
+  <?php endif; ?>
 </section>
